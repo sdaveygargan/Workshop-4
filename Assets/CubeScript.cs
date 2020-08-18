3809f028bcdfa5034d4b4895e00bee346c5e0c2d
Martin Reinoso <reinosom@unimelb.edu.au>
2020-08-18 19:57:36 +1000

    Innitial commit, same project form last year just open in Unity 2019.4.3f1

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        // Add a MeshFilter component to this entity. This essentially comprises of a
+        // mesh definition, which in this example is a collection of vertices, colours 
+        // and triangles (groups of three vertices). 
+        MeshFilter cubeMesh = this.gameObject.AddComponent<MeshFilter>();
+        cubeMesh.mesh = this.CreateCubeMesh();
+
+        // Add a MeshRenderer component. This component actually renders the mesh that
+        // is defined by the MeshFilter component. You will need to modify this 
+        // component for task 1 (and again for task 5), in order to set the material
+        // to be used in rendering the cube.
+        MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
+        //renderer.material ...
+    }
+
+    // Method to create a cube mesh with coloured vertices
+    Mesh CreateCubeMesh()
+    {
+        Mesh m = new Mesh();
+        m.name = "Cube";
+
+        // Define the vertices. These are the "points" in 3D space that allow us to
+        // construct 3D geometry (by connecting groups of 3 points into triangles).
+        m.vertices = new[] {
+            new Vector3(-1.0f, 1.0f, -1.0f), // Top
+            new Vector3(-1.0f, 1.0f, 1.0f),
+            new Vector3(1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, 1.0f, 1.0f),
+            new Vector3(1.0f, 1.0f, -1.0f),
+
+            new Vector3(-1.0f, -1.0f, -1.0f), // Bottom
+            new Vector3(1.0f, -1.0f, 1.0f),
+            new Vector3(-1.0f, -1.0f, 1.0f),
+            new Vector3(-1.0f, -1.0f, -1.0f),
+            new Vector3(1.0f, -1.0f, -1.0f),
+            new Vector3(1.0f, -1.0f, 1.0f),
+
+            new Vector3(-1.0f, -1.0f, -1.0f), // Left
+            new Vector3(-1.0f, -1.0f, 1.0f),
+            new Vector3(-1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, -1.0f, -1.0f),
+            new Vector3(-1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, 1.0f, -1.0f),
+
+            new Vector3(1.0f, -1.0f, -1.0f), // Right
+            new Vector3(1.0f, 1.0f, 1.0f),
+            new Vector3(1.0f, -1.0f, 1.0f),
+            new Vector3(1.0f, -1.0f, -1.0f),
+            new Vector3(1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, 1.0f, 1.0f),
+
+            new Vector3(-1.0f, 1.0f, 1.0f), // Front
+            new Vector3(1.0f, -1.0f, 1.0f),
+            new Vector3(1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, -1.0f, 1.0f),
+            new Vector3(1.0f, -1.0f, 1.0f),
+
+            new Vector3(-1.0f, 1.0f, -1.0f), // Back
+            new Vector3(1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, -1.0f, -1.0f),
+            new Vector3(-1.0f, -1.0f, -1.0f),
+            new Vector3(-1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, -1.0f, -1.0f)
+        };
+
+        // Define the vertex colours
+        m.colors = new[] {
+            Color.red, // Top
+            Color.red,
+            Color.red,
+            Color.red,
+            Color.red,
+            Color.red,
+
+            Color.red, // Bottom
+            Color.red,
+            Color.red,
+            Color.red,
+            Color.red,
+            Color.red,
+
+            Color.yellow, // Left
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+
+            Color.yellow, // Right
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+            Color.yellow,
+
+            Color.blue, // Front
+            Color.blue,
+            Color.blue,
+            Color.blue,
+            Color.blue,
+            Color.blue,
+
+            Color.blue, // Back
+            Color.blue,
+            Color.blue,
+            Color.blue,
+            Color.blue,
+            Color.blue
+        };
+
+        // Define the uv coordinates
+        m.uv = new[] {
+            new Vector2(0.0f, 0.666f), // Top
+            new Vector2(0.0f, 1.0f),
+            new Vector2(0.333f, 1.0f),
+            new Vector2(0.0f, 0.666f),
+            new Vector2(0.333f, 1.0f),
+            new Vector2(0.333f, 0.666f),
+
+            new Vector2(0.333f, 0.333f), // Bottom
+            new Vector2(0.666f, 0.0f),
+            new Vector2(0.333f, 0.0f),
+            new Vector2(0.333f, 0.333f),
+            new Vector2(0.666f, 0.333f),
+            new Vector2(0.666f, 0.0f),
+
+            new Vector2(0.0f, 0.0f), // Left
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+
+            new Vector2(0.0f, 0.333f), // Right
+            new Vector2(0.333f, 0.666f),
+            new Vector2(0.333f, 0.333f),
+            new Vector2(0.0f, 0.666f),
+            new Vector2(0.0f, 0.333f),
+            new Vector2(0.333f, 0.666f),
+
+            new Vector2(0.0f, 0.0f), // Front
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.666f, 0.666f),
+            new Vector2(0.666f, 0.333f),
+            new Vector2(0.333f, 0.333f),
+
+            new Vector2(0.0f, 0.333f), // Back
+            new Vector2(0.333f, 0.333f),
+            new Vector2(0.333f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 0.333f),
+            new Vector2(0.333f, 0.0f)
+        };
+
+        // Automatically define the triangles based on the number of vertices
+        int[] triangles = new int[m.vertices.Length];
+        for (int i = 0; i < m.vertices.Length; i++)
+            triangles[i] = i;
+
+        m.triangles = triangles;
+
+        return m;
+    }
+}
---
+++ Assets/CubeShader.shader
@@ -0,0 +1,41 @@
+﻿//UNITY_SHADER_NO_UPGRADE
+
+Shader "Unlit/CubeShader"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct vertIn
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct vertOut
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			// Implementation of the vertex shader
+			vertOut vert(vertIn v)
+			{
+				vertOut o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				return o;
+			}
+			
+			// Implementation of the fragment shader
+			fixed4 frag(vertOut v) : SV_Target
+			{
+				return float4(0.0f, 0.0f, 0.0f, 1.0f);
+			}
+			ENDCG
+		}
+	}
+}
